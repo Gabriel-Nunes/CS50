5afd54b6c1118e86d007d5b213ff5138cc2c557e
Gabriel-Nunes <31461719+Gabriel-Nunes@users.noreply.github.com>
2017-10-31 14:55:20 -0300

    Update vigenere.c

--- vigenere.c
+++ vigenere.c
@@ -1,66 +1,65 @@
 
 
-/* ************************************************* */
-/*                  CS50x - pset2                    */
-/*                                                   */
-/*                 V I G E N E R E                   */
-/*                                                   */
-/*              author - Gabriel Nunes               */
-/* ************************************************* */
-
 // this program uses the Vigenere Cypher to encipher
 // a text taken from user
 
-// OBS: this version is WITHOUT LOOPS
-
 #include <cs50.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <math.h>
 
 int main(int argc, string argv[])                    
 {
     string k = argv[1];    
-    int i = 0;
-    int j = 0;
     
-    while (islower (k[j]))
-    {
-        k[j] = (toupper (k[j]));
+    // assign argv[1] elements to string "k";
+    for (int a = 0, n = strlen(argv[1]); a < n; a++){
+        k[a] = argv[1][a];   
     }
     
-    if (argc == 2 && (isalpha(k[j])))
-    {
+    int j = strlen(k);
+    
+// I'll insert the argv[1] validation
+// here later.
+    
+    // the problem specification asks for only two comand line arguments;
+    if (argc == 2){
+        
         printf("plaintext: ");
+        // get the plaintext from user to be enciphered
+        // and store it in string "p";
         string p = get_string();                     
         printf("ciphertext: ");
         
-        if (isalpha(p[i]))
-        {
-            if (isupper(p[i]))
-            {
-                int c = ((p[i] + (k[j] -  65) - 65) % 26) + 65;
-                printf("%c", c);
+        // iterate between string "p" elements,
+        // and print the ciphered text stored in "c" variable;
+        for (int i = 0, n = strlen(p); i < n; i++){
+            int c = 0;
+            if (isalpha(p[i])) {
+                if (isupper(p[i]))
+                    {
+                        c = (((p[i] - 65) + (k[i % j] -  65)) % 26) + 65;
+                        printf("%c", c);
+                    }
+                    else if (islower(p[i]))
+                    {
+                        c = (((p[i] - 97) + (k[i % j] -  97)) % 26) + 97;
+                        printf("%c", c);
+                    }
             }
-            else if (islower(p[i]))
+            // if the p[i] element is not alpha, 
+            // just print it as it comes;
+            else
             {
-                int c = ((p[i] + (k[j] - 97) - 65) % 26) + 97;
-                printf("%c", c);
+                printf("%c", p[i]);
             }
         }
-        else
-        {
-            printf("%s", p);
-        }
-    
-    printf("\n");
-    return 0;
+        printf("\n");
+        return 0;
     }
     else                                 
-    {
-        printf("Usage: ./vigenere 'k' (aphabetical characters only)\n");
-        return 1;
-    }
+        {
+            printf("Usage: ./vigenere 'k' (aphabetical characters only)\n");
+            return 1;
+        }
 }
